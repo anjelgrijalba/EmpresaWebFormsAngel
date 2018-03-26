defbdaf8e42502000fa746f819ee76993ebdcb59
anjel <anggri@gmail.com>
2018-03-26 15:44:49 +0200

    bases de datos ok

---
+++ PresentacionWebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PresentacionWebForms.Startup))]
+namespace PresentacionWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
